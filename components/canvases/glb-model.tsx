8b10386428c5d1f6a4540eca7b27bfa0cc360c7b
Serhii Kuzmuch <sergiycheck@gmail.com>
2023-11-16 18:13:00 +0200

    feat: add animation

--- components/canvases/glb-model.tsx
+++ components/canvases/glb-model.tsx
@@ -1,7 +1,23 @@
-import {useGLTF} from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import * as THREE from "three";
 
-export default function GltfModel({link}: {link: string}) {
+export default function GltfModel({ link }: { link: string }) {
   const model = useGLTF(link);
+
+  const mixer = useRef<THREE.AnimationMixer>();
+  if (model.animations.length) {
+    mixer.current = new THREE.AnimationMixer(model.scene);
+    model.animations.forEach((clip) => {
+      const action = mixer.current?.clipAction(clip);
+      action?.play();
+    });
+  }
+
+  useFrame((state, delta) => {
+    mixer.current?.update(delta);
+  });
 
   return <>{model && <primitive object={model.scene} position={[0, 1, 0]} />}</>;
 }
