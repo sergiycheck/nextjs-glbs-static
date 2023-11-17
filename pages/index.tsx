7404188a4a92810a46a05aa8f02254dafcb7ec33
Serhii Kuzmuch <sergiycheck@gmail.com>
2023-11-17 13:15:43 +0200

    feat: changed names

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <main className={`flex f-full flex-col items-center justify-between ${inter.className}`}>
       {Array.from({ length: 100 }, (_, i) => i + 1).map((i) => (
-        <Link key={i} href={`/glbs/${i}`}>
-          <span className="mt-4 text-center">{i} glb</span>
+        <Link key={i} href={`/nfts/${i}`}>
+          <span className="mt-4 text-center">{i} nft</span>
         </Link>
       ))}
     </main>
